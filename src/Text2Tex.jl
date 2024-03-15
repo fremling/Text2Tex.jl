f854aa1052f0db534d03028015984b7ca0d99a67
Mikael Fremling <micke.fremling@gmail.com>
2024-03-15 10:07:52 +0100

    Updates the procedure.

--- src/Text2Tex.jl
+++ src/Text2Tex.jl
@@ -1,44 +1,49 @@
 module Text2Tex
 
-function Text2Tex(stringIn::AbstractString)
-    println("Decoding:'$stringIn'")    
-    charString=join([Text2Tex(char) for char in stringIn])
+export text2tex
+
+function text2tex(stringIn::AbstractString;verbose=false)
+    verbose && println("Decoding:'$stringIn'")    
+    charString=join([text2tex(char) for char in stringIn])
     ##Search for <sub>
-    charString = replace(charString, "<sub>"=>"\$_{")
-    #println("Replaced:'$string'")
-    charString = replace(charString, "</sub>"=>"}\$")
-    #println("Replaced:'$string'")
+    charString = replace(charString, "<sub>"=>"\\textsubscript{")
+    charString = replace(charString, "<sup>"=>"\\textsuperscript{")
+    charString = replace(charString, "</sub>"=>"}")
+    charString = replace(charString, "</sup>"=>"}")
     charString = replace(charString, "<br>"=>"\\\\")
     return charString
 end
 
 function BO(accentcode,letter::Char)
-    return "{"*'\\'*accentcode*letter*"}"
+    return "{{"*'\\'*accentcode*"}"*letter*"}"
 end
 
-Text2Tex(::Missing) = ""
-function Text2Tex(c::Char)
+text2tex(::Missing) = ""
+function text2tex(c::Char)
     in(c,'A':'Z') && return c ### Upper case
     in(c,'a':'z') && return c ### lower case
     in(c,'0':'9') && return c ### lower case
     in(c,".;,():_@/<>{}") && return c
     c == '\$' && return c
-    c == '&' && return "\\&"  ###
+
+    c == 'å' && return "{\\aa}"  ###
+    c == '&' && return "{\\&}"  ###
     c == '-' && return '-' ###Long dash
     c == '–' && return '-' ###Long dash
     c == '’' && return '\'' ###Fancy end-quite
 
     c == '€' && return "{\\euro}"
     #https://tex.stackexchange.com/tags/accents/info
+    #https://tex.stackexchange.com/questions/8857/how-to-type-special-accented-letters-in-latex
     
     isspace(c) && return c # space
     ###Special characters
     c == 'ı' && return 'i'  ###FIXME
     c == 'ł' && return 'l'  ###FIXME
-    c == 'ß' && return BO('s','s')
+    c == 'ß' && return "{\\ss}"
     ## cedilla
-    c == 'ş' && return 's' ###FIXME return BO('c','s')
-    c == 'ç' && return 'c' ###FIXME BO('c','c')
+    c == 'ş' && return BO('c','s')
+    c == 'ç' && return BO('c','c')
     ## check
     c == 'ğ' && return "g" ###FIXME '\\'*'v'*'g'
     c == 'Š' && return "S" ###FIXME '\\'*'v'*'S'
@@ -48,6 +53,7 @@ function Text2Tex(c::Char)
     c == 'ö' && return BO('"','o')
     c == 'ë' && return BO('"','e')
     c == 'ü' && return BO('"','u')
+    c == 'ä' && return BO('"','a')
     ## grave accent
     c == 'è' && return BO("\`",'e')
     c == 'à' && return BO("\`",'a')
@@ -67,7 +73,4 @@ function Text2Tex(c::Char)
 end
 
 
-
-@test  Text2Tex("Wte<sub>2</sub>B") == "Wte\$_{2}\$B"
-@test  Text2Tex("Wte<sub>1</sub>") == "Wte\$_{1}\$"
-@test  Text2Tex("G<sub>10</sub>A") == "G\$_{10}\$A"
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,21 @@
 
 using Test
 
-@test Text2Tex("A<sub>B</sub>"=>"A\\textsubscript{B}$")
-@test Text2Tex("A<br>B"=>"A\\\\B")
+using Text2Tex
 
-@test Text2Tex("&’"=>"\\&\'")
+@test text2tex("A<sub>B</sub>") == "A\\textsubscript{B}"
+@test text2tex("A<br>B") == "A\\\\B"
 
-@test  Text2Tex("Wte<sub>2</sub>B") == "Wte\\textsubscript{2}B"
-@test  Text2Tex("Wte<sub>1</sub>") == "Wte\\textsubscript{1}"
-@test  Text2Tex("G<sub>10</sub>A") == "G\\textsubscript{10}A"
-@test  Text2Tex("G<sup>10</sup>A") == "G\\textsuperscript{10}A"
+@test  text2tex("Wte<sub>2</sub>B") == "Wte\\textsubscript{2}B"
+@test  text2tex("Wte<sub>1</sub>") == "Wte\\textsubscript{1}"
+@test  text2tex("G<sub>10</sub>A") == "G\\textsubscript{10}A"
+@test  text2tex("G<sup>10</sup>A") == "G\\textsuperscript{10}A"
+
+@test text2tex("&’") == "{\\&}\'"
+
+
+@test text2tex("åäö") == "{\\aa}{{\\\"}a}{{\\\"}o}"
+
+@test text2tex("Straße") == "Stra{\\ss}e"
+
+@test text2tex("ş") == "{{\\c}s}"
