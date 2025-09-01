00115d954367ecc0449a25bcf0fb36da23c140bc
Mikael Fremling <micke.fremling@gmail.com>
2025-09-01 14:41:13 +0200

    Added longer message

--- src/Text2Tex.jl
+++ src/Text2Tex.jl
@@ -2,9 +2,30 @@ module Text2Tex
 
 export text2tex
 
-function text2tex(stringIn::AbstractString;verbose=false)
-    verbose && println("Decoding:'$stringIn'")    
-    charString=join([text2tex(char) for char in stringIn])
+function text2tex(stringIn::AbstractString;verbose=false,context=10::Integer)
+    verbose && println("Decoding:'$stringIn'")
+    ErrorStr=""
+    charString = try join([text2tex(char) for char in stringIn])
+    catch y
+        ###Do character by character
+        str = ""
+        for (cno,char) in enumerate(stringIn) ###Try again one character at a time
+            #println(cno," char:",char)
+            t2tc = try text2tex(char)
+            catch ### Catch the error and display the context
+                noLeft = minimum([cno-1,context])
+                noRight = minimum([length(stringIn)-cno,context])
+                StringLeft=stringIn[(cno-noLeft):(cno-1)]
+                #StringRight=stringIn[(cno+1):(cno+noRight)]
+                ErrorStr="Unknown character in \""*StringLeft*"\"+'"*char*"'"*". Cannot decode to TeX"
+                break
+            end
+        end
+    end
+    if ErrorStr != ""
+        error(ErrorStr)
+    end
+        
     ##Search for <sub>
     charString = replace(charString, "<sub>"=>"\\textsubscript{")
     charString = replace(charString, "<sup>"=>"\\textsuperscript{")
